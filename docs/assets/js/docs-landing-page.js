df0fd1ce98c39fe560c3f733e8f6eaebe91cc949
ryanlambda <ryan.martin@lambdal.com>
2024-09-26 13:42:43 -0700

    Add try/catch

--- docs/assets/js/docs-landing-page.js
+++ docs/assets/js/docs-landing-page.js
@@ -1,23 +1,26 @@
 function makeCardsClickable() {
     let cards = document.querySelector('.cards').querySelectorAll('li');
     for (let i = 0; i < cards.length; i++) {
-        // Get card content
-        let link = cards[i].querySelector('a').href;
-        let content = cards[i].innerHTML;
+        try {
+            // Get card content
+            let link = cards[i].querySelector('a').href;
+            let content = cards[i].innerHTML;
 
-        // Wrap card content
-        let a = document.createElement('a');
-        let span = document.createElement('span');
-        a.href = link;
-        a.innerHTML = content;
-        span.className = 'll-card-wrapper';
-        span.appendChild(a);
+            // Wrap card content
+            let a = document.createElement('a');
+            let span = document.createElement('span');
+            a.href = link;
+            a.innerHTML = content;
+            span.className = 'll-card-wrapper';
+            span.appendChild(a);
 
-        // Replace old content with wrapped content
-        cards[i].innerHTML = '';
-        cards[i].appendChild(span);
+            // Replace old content with wrapped content
+            cards[i].innerHTML = '';
+            cards[i].appendChild(span);
+        } catch (e) {}
     }
 }
 document.addEventListener('DOMContentLoaded', (e) => {
     makeCardsClickable();
 });
+

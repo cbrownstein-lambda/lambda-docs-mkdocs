74055d097a43ec7d0027fdc3fae6738212654120
ryanlambda <ryanlambda@users.noreply.github.com>
2024-10-09 23:21:21 +0000

    Deployed a4f3b5b with MkDocs version: 1.6.1

---
+++ assets/js/docs-landing-page.js
@@ -0,0 +1,26 @@
+function makeCardsClickable() {
+    let cards = document.querySelector('.cards').querySelectorAll('li');
+    for (let i = 0; i < cards.length; i++) {
+        try {
+            // Get card content
+            let link = cards[i].querySelector('a').href;
+            let content = cards[i].innerHTML;
+
+            // Wrap card content
+            let a = document.createElement('a');
+            let span = document.createElement('span');
+            a.href = link;
+            a.innerHTML = content;
+            span.className = 'll-card-wrapper';
+            span.appendChild(a);
+
+            // Replace old content with wrapped content
+            cards[i].innerHTML = '';
+            cards[i].appendChild(span);
+        } catch (e) {}
+    }
+}
+document.addEventListener('DOMContentLoaded', (e) => {
+    makeCardsClickable();
+});
+
